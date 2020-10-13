7816b1afc207467351a6088990e1401040b3ed79
Alonso <alonso.serrano@leanbusinessg.com>
2020-10-13 16:51:25 -0500

    Chat final

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,11 +1,8 @@
 var socket = io();
 
-
-
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
 }
@@ -13,15 +10,18 @@ if (!params.has('nombre') || !params.has('sala')) {
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+    });
 
-        console.log('usuarios Conectados', resp);
-    })
 });
 
 // escuchar
@@ -33,29 +33,29 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-
-/*
-socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//escuchar cambios del usuario
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log('Servidor:', personas);
+    renderizarUsuarios(personas);
 });
 
-//Mesajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('mensaje Prvado', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -28,12 +28,17 @@ io.on('connection', (client) => {
 
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         let persona = usuarios.getPersona(client.id);
 
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', crearMensaje('Administrador', `${ persona.nombre } se unio`));
+
+        callback(mensaje);
+
+
     });
 
     client.on('disconnect', () => {
